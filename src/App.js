c1a92b4cd2662bbc048e515634ddb44bd53c963e
Jungma1 <mhg3630@naver.com>
2021-02-26 22:55:38 +0900

    props 컴포넌트 기본

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
+import MyComponent from './MyComponent';
 
 function App() {
-  const name = 'React';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: 16,
-  };
-
-  return <>{name === 'React' && <h1 style={style}>React 입니다.</h1>}</>;
+	return <MyComponent name='Mellang' />
 }
 
 export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyComponent = (props) => {
+  const { name } = props;
+
+  return <div>First Component, [ {name} ]!</div>;
+};
+
+export default MyComponent;
