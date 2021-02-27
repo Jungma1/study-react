ac60135098d0e72c24ff30c2d632449c9132eff1
Jungma1 <mhg3630@naver.com>
2021-02-27 20:13:14 +0900

    [Hooks] useState - 여러 state 사용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Counter from './hooks/Counter';
+import Info from './hooks/Info';
 
 function App() {
-	return <Counter />
+	return <Info />
 }
 
 export default App;
