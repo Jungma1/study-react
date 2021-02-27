ad21b4359fa9ed52e5ab894f70105eb0df2e559d
Jungma1 <mhg3630@naver.com>
2021-02-27 22:28:12 +0900

    [Hooks] useEffect - 사용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Info from './hooks/Info';
 
 function App() {
-	return <Info />
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
 }
 
 export default App;
