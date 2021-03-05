c8e9dcca8dd52e3c9c84bec0858bb57caa730d8e
Jungma1 <mhg3630@naver.com>
2021-03-05 18:36:57 +0900

    [Hooks] useReducer - Counter Component 수정

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import React, { useState } from 'react';
-import Info from './hooks/Info';
+import React from 'react';
+import Counter from './hooks/Counter';
 
 function App() {
-  const [visible, setVisible] = useState(false);
-
   return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
+		<Counter />
   );
 }
 
