7ab26d3c6897872233a00b3803453060edd0af6b
Jungma1 <mhg3630@naver.com>
2021-03-21 23:32:39 +0900

    clear 3/21

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoInsert from './todos/components/TodoInsert';
+import TodoList from './todos/components/TodoList';
 import TodoTemplate from './todos/components/TodoTemplate';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: `리액트의 기초 알아보기`,
+      checked: true,
+    },
+  ])
+
   return (
     <TodoTemplate>
-      Add todo!!
+      <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 }
